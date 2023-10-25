3bb310cf66760fc4e7aba13d3a8ade66ab9bf4bd
Michał Bzowski <michal.bzowski@gmail.com>
2023-10-25 16:33:33 +0200

    Update test.py

--- test.py
+++ test.py
@@ -34,124 +34,128 @@ class TestBooklet(unittest.TestCase):
         b = Booklet(5, 2)
         self.assertEqual(b.booklet_pages_count, 8)
 
-    # def test_one_image_should_be_at_right_side_of_first_booklet_page(self):
-    #     b = Booklet(1, 1)
-    #     images = ["01.jpg"]
+    def test_one_image_should_be_at_right_side_of_first_booklet_page(self):
+        b = Booklet(1, 1)
+        images = ["01.jpg"]
         
-    #     b.assign_images(images)
+        b.assign_images(images)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "01.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
+        self.assertEqual(next_image.booklet_sheet, 1)
 
-    # def test_second_image_should_be_at_left_side_of_second_booklet_page(self):
-    #     b = Booklet(2, 1)
-    #     images = ["01.jpg", "02.jpg"]
+    def test_second_image_should_be_at_left_side_of_second_booklet_page(self):
+        b = Booklet(2, 1)
+        images = ["01.jpg", "02.jpg"]
         
-    #     b.assign_images(images)
+        b.assign_images(images)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "01.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 1)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
+        self.assertEqual(next_image.booklet_sheet, 1)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "02.jpg")
-    #     self.assertEqual(next_image.edge, self.LEWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 2)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "02.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
+        self.assertEqual(next_image.booklet_sheet, 1)
 
-    # def test_third_image_should_be_at_right_side_of_second_booklet_page(self):
-    #     b = Booklet(3, 1)
-    #     images = ["01.jpg", "02.jpg", "03.jpg"]
+    def test_third_image_should_be_at_right_side_of_second_booklet_page(self):
+        b = Booklet(3, 1)
+        images = ["01.jpg", "02.jpg", "03.jpg"]
         
-    #     b.assign_images(images)
+        b.assign_images(images)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "01.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 1)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "02.jpg")
-    #     self.assertEqual(next_image.edge, self.LEWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 2)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "02.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "03.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 2)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "03.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
 
-    # def test_fourth_image_should_be_at_left_side_of_first_booklet_page(self):
-    #     b = Booklet(4, 1)
-    #     images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"]
+    def test_fourth_image_should_be_at_left_side_of_first_booklet_page(self):
+        b = Booklet(4, 1)
+        images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"]
         
-    #     b.assign_images(images)
+        b.assign_images(images)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "01.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 1)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "02.jpg")
-    #     self.assertEqual(next_image.edge, self.LEWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 2)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "02.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "03.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 2)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "03.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "04.jpg")
-    #     self.assertEqual(next_image.edge, self.LEWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 1)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "04.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
 
-    # def test_fifth_image_should_be_at_right_side_of_third_booklet_page(self):
-    #     b = Booklet(5, 1)
-    #     images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"]
+    def test_fifth_image_should_be_at_right_side_of_third_booklet_page(self):
+        b = Booklet(5, 1)
+        images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"]
         
-    #     b.assign_images(images)
+        b.assign_images(images)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "01.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 1)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "02.jpg")
-    #     self.assertEqual(next_image.edge, self.LEWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 2)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "02.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "03.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 3)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "03.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 3)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "04.jpg")
-    #     self.assertEqual(next_image.edge, self.LEWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 4)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "04.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 4)
 
-    #     next_image = b.return_next_image()
-    #     self.assertEqual(next_image.path, "05.jpg")
-    #     self.assertEqual(next_image.edge, self.PRAWA)
-    #     self.assertEqual(next_image.y, 0)
-    #     self.assertEqual(next_image.booklet_page, 4)
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "05.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 4)
 
 
     def test_sixth_image_should_be_at_left_side_of_fourth_booklet_page(self):
@@ -237,6 +241,7 @@ class TestBooklet(unittest.TestCase):
         self.assertEqual(next_image.booklet_page, 4)
         self.assertEqual(next_image.booklet_sheet, 2)
 
+########
 
         next_image = b.return_next_image()
         self.assertEqual(next_image.path, "05.jpg")
@@ -258,6 +263,79 @@ class TestBooklet(unittest.TestCase):
         self.assertEqual(next_image.y, 0)
         self.assertEqual(next_image.booklet_page, 2)
         self.assertEqual(next_image.booklet_sheet, 1)
+
+    def nine(self):
+        b = Booklet(7, 1)
+        images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg"]
+        
+        b.assign_images(images)
+
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
+        self.assertEqual(next_image.booklet_sheet, 1)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "02.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
+        self.assertEqual(next_image.booklet_sheet, 1)
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "03.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 3)
+        self.assertEqual(next_image.booklet_sheet, 2)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "04.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 4)
+        self.assertEqual(next_image.booklet_sheet, 3)
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "05.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 5)
+        self.assertEqual(next_image.booklet_sheet, 3)
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "06.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 6)
+        self.assertEqual(next_image.booklet_sheet, 3)
+
+#######
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "07.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 6)
+        self.assertEqual(next_image.booklet_sheet, 3)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "08.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 5)
+        self.assertEqual(next_image.booklet_sheet, 3)
+
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "09.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 4)
+        self.assertEqual(next_image.booklet_sheet, 2)
 
 
 
