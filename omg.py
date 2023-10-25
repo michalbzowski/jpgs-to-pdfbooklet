c27fe7837397bce001ce0d959cc27a4f1208b108
Michał Bzowski <michal.bzowski@gmail.com>
2023-10-25 16:33:51 +0200

    Update omg.py

--- omg.py
+++ omg.py
@@ -24,84 +24,72 @@ class Booklet:
         self.images_in_booklet = []
         self.currently_returnet_image = 0
         current_sheet = 1
-        if self.booklet_sheets_count == 1:
-            i = 1
-            current_page = 1
-            for image in images:
-                horizontal_snap = i % 2
-                self.images_in_booklet.append(BookletImage(image, horizontal_snap, 0, current_page, current_sheet, "A"))
-                i = i + 1
-                if i % 2 == 0:
-                    current_page = current_page + 1
-                if i % 4 == 0:
-                    current_page = current_page - 2
-        elif self.booklet_sheets_count > 1:
-            i = 1
-            current_page = 1
-            direction = 1
-            RIGHT = 1
-            LEFT = 0
-            edge = -1
-            for n in range(0, int(self.booklet_pages_count/2)):
-                if n >= len(images): 
-                    break
-                image = images[n]
-                if i % 2 == 0:
-                    edge = LEFT
-                else:
-                    edge = RIGHT
-                self.images_in_booklet.append(BookletImage(image, edge, 0, current_page, current_sheet, "A"))
-                i = i + 1
-                if n == int(self.booklet_pages_count/2) - 1:
-                    break
-                else:
-                    current_page = current_page + 1
-                if i % 2 == 1:
-                    current_sheet = current_sheet + 1
+        i = 1
+        current_page = 1
+        direction = 1
+        RIGHT = 1
+        LEFT = 0
+        edge = -1
+        for n in range(0, int(self.booklet_pages_count/2)):
+            if n >= len(images): 
+                break
+            image = images[n]
+            if i % 2 == 0:
+                edge = LEFT
+            else:
+                edge = RIGHT
+            self.images_in_booklet.append(BookletImage(image, edge, 0, current_page, current_sheet, "A"))
+            i = i + 1
+            if n == int(self.booklet_pages_count/2) - 1:
+                break
+            else:
+                current_page = current_page + 1
+            if i % 2 == 1:
+                current_sheet = current_sheet + 1
 
-            for n in range(int(self.booklet_pages_count/2), self.booklet_pages_count):
-                if n >= len(images): 
-                    break
-                image = images[n]
-                if i % 2 == 0:
-                    edge = LEFT
-                else:
-                    edge = RIGHT
-                self.images_in_booklet.append(BookletImage(image, edge, 0, current_page, current_sheet, "A"))
-                if n == self.booklet_pages_count:
-                    break
-                i = i + 1
-                current_page = current_page - 1
-                if i % 2 == 1:
-                    current_sheet = current_sheet - 1
+        for n in range(int(self.booklet_pages_count/2), self.booklet_pages_count):
+            if n >= len(images): 
+                break
+            image = images[n]
+            if i % 2 == 0:
+                edge = LEFT
+            else:
+                edge = RIGHT
+            self.images_in_booklet.append(BookletImage(image, edge, 0, current_page, current_sheet, "A"))
+            if n == self.booklet_pages_count:
+                break
+            i = i + 1
+            current_page = current_page - 1
+            if i % 2 == 1:
+                current_sheet = current_sheet - 1
 
-            
-                # if current_sheet != self.booklet_sheets_count:
-                #     imageA = images[n]
-                #     self.images_in_booklet.append(BookletImage(imageA, right, 0, current_page, current_sheet, "A"))
-                #     if n + 1 >= len(images):
-                #         break
-                #     imageB = images[n + 1]
-                #     current_page = current_page + 1
-                #     self.images_in_booklet.append(BookletImage(imageB, left, 0, current_page, current_sheet, "B"))
-                #     current_page = current_page + 1
-                #     current_sheet = current_sheet + direction
-                #     continue
-            
-                # if current_sheet == self.booklet_sheets_count:
-                #     imageA = images[n]
-                #     self.images_in_booklet.append(BookletImage(imageA, right, 0, current_page, current_sheet, "A" if the_same_sheet_two_times == 0 else "B"))
-                #     current_page = current_page + 1
-                #     if n + 1 >= len(images):
-                #         break
-                #     the_same_sheet_two_times = the_same_sheet_two_times + 1
-                #     if the_same_sheet_two_times == 2:
-                #         direction = -1
-                #         current_page = current_page + direction
-                #     imageB = images[n + 1]
-                #     self.images_in_booklet.append(BookletImage(imageB, left, 0, current_page, current_sheet, "B" if the_same_sheet_two_times == 0 else "A"))
-                #     if the_same_sheet_two_times == 2:
-                #         current_sheet = current_sheet + direction
+        
+            # if current_sheet != self.booklet_sheets_count:
+            #     imageA = images[n]
+            #     self.images_in_booklet.append(BookletImage(imageA, right, 0, current_page, current_sheet, "A"))
+            #     if n + 1 >= len(images):
+            #         break
+            #     imageB = images[n + 1]
+            #     current_page = current_page + 1
+            #     self.images_in_booklet.append(BookletImage(imageB, left, 0, current_page, current_sheet, "B"))
+            #     current_page = current_page + 1
+            #     current_sheet = current_sheet + direction
+            #     continue
+        
+            # if current_sheet == self.booklet_sheets_count:
+            #     imageA = images[n]
+            #     self.images_in_booklet.append(BookletImage(imageA, right, 0, current_page, current_sheet, "A" if the_same_sheet_two_times == 0 else "B"))
+            #     current_page = current_page + 1
+            #     if n + 1 >= len(images):
+            #         break
+            #     the_same_sheet_two_times = the_same_sheet_two_times + 1
+            #     if the_same_sheet_two_times == 2:
+            #         direction = -1
+            #         current_page = current_page + direction
+            #     imageB = images[n + 1]
+            #     self.images_in_booklet.append(BookletImage(imageB, left, 0, current_page, current_sheet, "B" if the_same_sheet_two_times == 0 else "A"))
+            #     if the_same_sheet_two_times == 2:
+            #         current_sheet = current_sheet + direction
                         
                     
 
