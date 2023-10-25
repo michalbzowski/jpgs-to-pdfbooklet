db01985f2cba222ef4e1793f3205b8856ff46d02
michal.bzowski@gmail.com <BJZ)tp,Z]n9{iAq5(y"k&F*\9"s'bfVt4j(6:\VJ)Q&*5"{%h4@i~Z6G]#.cF7E>
2023-10-26 01:34:43 +0200

    Something works

--- omg.py
+++ omg.py
@@ -1,10 +1,11 @@
 import math
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.pagesizes import landscape
+from reportlab.pdfgen import canvas
+import os
 
-def sum(arg):
-    total = 0
-    for val in arg:
-        total += val
-    return total
+dir_to_lookup = "C:\\Users\\Monika Bzowska\\IdeaProjects\\band-book-maker-main-kosicelne\\src\\main\\resources\\NUTY_KOSCIELNE"
+empty_page_path = "C:\\Users\\Monika Bzowska\\IdeaProjects\\band-book-maker-main-kosicelne\\src\\main\\resources\\empty_page.jpg"
 
 class Booklet:
     """Booklet representation"""
@@ -14,11 +15,13 @@ class Booklet:
     images_in_booklet = []
     currently_returnet_image = 0
     booklet_sheets_count = 0
+    images_count = 0
 
     def __init__(self, images_count, documents_per_page):
         self.documents_per_page = documents_per_page
         self.booklet_pages_count = math.ceil(images_count / 4) * 4 ## Założenie, że zawsze parzyste
         self.booklet_sheets_count = math.ceil(images_count / 4)
+        self.images_count = images_count
 
     def assign_images(self, images):
         self.images_in_booklet = []
@@ -26,7 +29,6 @@ class Booklet:
         current_sheet = 1
         i = 1
         current_page = 1
-        direction = 1
         RIGHT = 1
         LEFT = 0
         edge = -1
@@ -63,42 +65,18 @@ class Booklet:
             if i % 2 == 1:
                 current_sheet = current_sheet - 1
 
-        
-            # if current_sheet != self.booklet_sheets_count:
-            #     imageA = images[n]
-            #     self.images_in_booklet.append(BookletImage(imageA, right, 0, current_page, current_sheet, "A"))
-            #     if n + 1 >= len(images):
-            #         break
-            #     imageB = images[n + 1]
-            #     current_page = current_page + 1
-            #     self.images_in_booklet.append(BookletImage(imageB, left, 0, current_page, current_sheet, "B"))
-            #     current_page = current_page + 1
-            #     current_sheet = current_sheet + direction
-            #     continue
-        
-            # if current_sheet == self.booklet_sheets_count:
-            #     imageA = images[n]
-            #     self.images_in_booklet.append(BookletImage(imageA, right, 0, current_page, current_sheet, "A" if the_same_sheet_two_times == 0 else "B"))
-            #     current_page = current_page + 1
-            #     if n + 1 >= len(images):
-            #         break
-            #     the_same_sheet_two_times = the_same_sheet_two_times + 1
-            #     if the_same_sheet_two_times == 2:
-            #         direction = -1
-            #         current_page = current_page + direction
-            #     imageB = images[n + 1]
-            #     self.images_in_booklet.append(BookletImage(imageB, left, 0, current_page, current_sheet, "B" if the_same_sheet_two_times == 0 else "A"))
-            #     if the_same_sheet_two_times == 2:
-            #         current_sheet = current_sheet + direction
-                        
-                    
-
-
     
     def return_next_image(self):
         return_now = self.currently_returnet_image
         self.currently_returnet_image = self.currently_returnet_image + 1
         return self.images_in_booklet[return_now]
+    
+    def return_images_with_page(self, page_number):
+        pages = []
+        for image in self.images_in_booklet:
+            if image.booklet_page == page_number:
+                pages.append(image)
+        return pages
 
 class BookletImage:
     """One imge in booklet - so it is one file with coordinates where to place it"""
@@ -116,3 +94,66 @@ class BookletImage:
         self.booklet_page = booklet_page
         self.booklet_sheet = booklet_sheet
         self.booklet_side = booklet_side
+
+class Printer:
+    "Class for take preapared Booklet implementation and write it to PDF file"
+
+    def save(self, booklet: Booklet, some_name):
+        wi = A4[0]/2
+        hi = A4[1]/2
+        # Otwórz plik PDF wynikowy
+        output_pdf = some_name +"_wyjściowy.pdf"
+        c = canvas.Canvas(output_pdf, pagesize=A4)
+        max_page_number = -9999
+        for booklet_image in booklet.images_in_booklet:
+            if booklet_image.booklet_page > max_page_number:
+                max_page_number = booklet_image.booklet_page
+
+        for n in range(1, max_page_number + 1):
+            pages = booklet.return_images_with_page(n)
+            if len(pages) > 0:
+                y = hi
+                img = pages[0]
+                c.drawImage(img.path, wi if img.edge == 1 else 0, y, wi, hi, preserveAspectRatio=True)
+            if len(pages) > 1:
+                y = hi
+
+                img = pages[1]
+                c.drawImage(img.path, wi if img.edge == 1 else 0, y, wi, hi, preserveAspectRatio=True)
+            c.showPage()
+        
+        c.save()
+        print(f"Plik PDF {output_pdf} został utworzony z plików JPG.")
+
+def list_jpgs_from_dir(dir_name):
+    return [file for file in os.listdir(dir_name) if file.endswith('.jpg')]
+
+def list_jpg_names(dir_name):
+    jpg_files = list_jpgs_from_dir(dir_name)
+    files_in_read_order = []
+    for jpg_file in jpg_files:
+        #Zapisz sciezke do pliku w tablicy
+        files_in_read_order.append(os.path.join(dir_name, jpg_file))
+    return files_in_read_order
+
+def fill_with_empty_page(count):
+    a = []
+    for n in range(0, count):
+        a.append(empty_page_path)
+    return a
+
+dir_list = []
+for dir_path in os.listdir(dir_to_lookup):
+    dir_list.append(os.path.join(dir_to_lookup, dir_path))
+
+for p in range(0, len(dir_list)):
+    print(".")
+    dir_name = dir_list[p]
+    files_in_read_order_a = list_jpg_names(dir_name)
+    b = Booklet(len(files_in_read_order_a), 1)
+    b.assign_images(files_in_read_order_a)
+    p = Printer()
+    p.save(b, dir_name)       
+    pass
+
+
--- test.py
+++ test.py
@@ -9,14 +9,6 @@ class TestBooklet(unittest.TestCase):
     A = "A"
     B = "B"
 
-    def test_list_int(self):
-        """
-        Test that it can sum a list of integers
-        """
-        data = [1, 2, 3]
-        result = sum(data)
-        self.assertEqual(result, 6)
-
         
     def test_booklet_pages_count_should_be_four(self):
         b = Booklet(1, 1)
@@ -337,6 +329,207 @@ class TestBooklet(unittest.TestCase):
         self.assertEqual(next_image.booklet_page, 4)
         self.assertEqual(next_image.booklet_sheet, 2)
 
+    def test_current_koscielne_ksiazeczki(self):
+        b = Booklet(26, 1)
+        images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg", "09.jpg", 
+                  "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg",
+                  "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg"]
+        
+        b.assign_images(images)
+
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "01.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 1)
+        self.assertEqual(next_image.booklet_sheet, 1)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "02.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 2)
+        self.assertEqual(next_image.booklet_sheet, 1)
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "03.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 3)
+        self.assertEqual(next_image.booklet_sheet, 2)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "04.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 4)
+        self.assertEqual(next_image.booklet_sheet, 2)
+###
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "05.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 5)
+        self.assertEqual(next_image.booklet_sheet, 3)
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "06.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 6)
+        self.assertEqual(next_image.booklet_sheet, 3)
+
+
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "07.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 7)
+        self.assertEqual(next_image.booklet_sheet, 4)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "08.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 8)
+        self.assertEqual(next_image.booklet_sheet, 4)
+
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "09.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 9)
+        self.assertEqual(next_image.booklet_sheet, 5)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "10.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 10)
+        self.assertEqual(next_image.booklet_sheet, 5)
+
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "11.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 11)
+        self.assertEqual(next_image.booklet_sheet, 6)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "12.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 12)
+        self.assertEqual(next_image.booklet_sheet, 6)
+
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "13.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 13)
+        self.assertEqual(next_image.booklet_sheet, 7)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "14.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 14)
+        self.assertEqual(next_image.booklet_sheet, 7)
+
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "15.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 14)
+        self.assertEqual(next_image.booklet_sheet, 7)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "16.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 13)
+        self.assertEqual(next_image.booklet_sheet, 7)
+
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "17.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 12)
+        self.assertEqual(next_image.booklet_sheet, 6)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "18.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page,11)
+        self.assertEqual(next_image.booklet_sheet, 6)
+
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "19.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 10)
+        self.assertEqual(next_image.booklet_sheet, 5)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "20.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 9)
+        self.assertEqual(next_image.booklet_sheet, 5)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "21.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 8)
+        self.assertEqual(next_image.booklet_sheet, 4)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "22.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 7)
+        self.assertEqual(next_image.booklet_sheet, 4)
+
+        
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "23.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 6)
+        self.assertEqual(next_image.booklet_sheet, 3)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "24.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 5)
+        self.assertEqual(next_image.booklet_sheet, 3)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "25.jpg")
+        self.assertEqual(next_image.edge, self.PRAWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 4)
+        self.assertEqual(next_image.booklet_sheet, 2)
+
+        next_image = b.return_next_image()
+        self.assertEqual(next_image.path, "26.jpg")
+        self.assertEqual(next_image.edge, self.LEWA)
+        self.assertEqual(next_image.y, 0)
+        self.assertEqual(next_image.booklet_page, 3)
+        self.assertEqual(next_image.booklet_sheet, 2)
+
+        #R2,1 i L1,1 są puste - przetestować to?
 
 
 if __name__ == '__main__':
